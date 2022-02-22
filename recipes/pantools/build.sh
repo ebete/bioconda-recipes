b7e3aeee1f79d2ba581398bd066b596bf6415af6
Thom Griffioen <thomgriffioen@gmail.com>
2022-02-22 10:12:51 +0100

    Use git_url for pantools
    This is necessary for the new git submodule dependency

--- recipes/pantools/build.sh
+++ recipes/pantools/build.sh
@@ -6,6 +6,14 @@ PACKAGE_HOME="${PREFIX}/share/${PKG_NAME}"
 mkdir -p "${PREFIX}/bin"
 mkdir -p "${PACKAGE_HOME}"
 
+
+# ASTER dependency setup
+git submodule update --init --recursive
+(
+	cd ASTER
+	g++ -std=gnu++11 -march=native -Ofast -pthread astral.cpp -o astral
+	g++ -std=gnu++11 -march=native -Ofast -pthread astral-pro.cpp -o astral-pro
+)
 # https://git.wur.nl/bioinformatics/pantools/-/blob/a96f1dcbd6273ce484c513c42c3bb7c3d8abe119/README.md#building-a-runnable-jar
 # this will generate 2 JAR files:
 # * 'original-pantools-X.Y.Z.jar'
