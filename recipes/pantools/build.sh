9e941607ee13d9f9ca765619d6479b9178b6ab26
Thom Griffioen <thomgriffioen@gmail.com>
2022-02-23 16:13:31 +0100

    use CXX instead of hardcoded compiler

--- recipes/pantools/build.sh
+++ recipes/pantools/build.sh
@@ -11,8 +11,8 @@ mkdir -p "${PACKAGE_HOME}"
 git submodule update --init --recursive
 (
 	cd ASTER
-	g++ -std=gnu++11 -march=native -Ofast -pthread astral.cpp -o astral
-	g++ -std=gnu++11 -march=native -Ofast -pthread astral-pro.cpp -o astral-pro
+	$CXX -std=gnu++11 -march=native -Ofast -pthread astral.cpp -o astral
+	$CXX -std=gnu++11 -march=native -Ofast -pthread astral-pro.cpp -o astral-pro
 )
 # https://git.wur.nl/bioinformatics/pantools/-/blob/a96f1dcbd6273ce484c513c42c3bb7c3d8abe119/README.md#building-a-runnable-jar
 # this will generate 2 JAR files:
